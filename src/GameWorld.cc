6781518526b7c88994a4a0e0322cb08404f2f55a
NemesisShooter <grekdarkness@gmail.com>
2016-01-07 14:24:42 +0000

    Creating more cubes

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -2,7 +2,9 @@
 
 GameWorld::GameWorld (ApplicationMode mode) : asset_manager(std::make_shared<GameAssetManager>(mode)) {
   asset_manager->AddAsset(std::make_shared<CubeAsset>(0,0,0));
-	//making new cubes
+	/*making new cubes, if there is 0 cubes then there will be 1 created
+		if there is more then 1 cube but less then 3 additional cubes
+	*/will be created and abbd with new coordinates
 	for(int i=0; i<3; i++)
 	{
 		if(i == 0)
